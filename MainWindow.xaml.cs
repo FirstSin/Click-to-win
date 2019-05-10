44bf45255c695abbf1f1813fe58cb83a89bca94a
FirstSin <firstsind@gmail.com>
2019-05-11 01:34:09 +0300

    Add a simple interface
    Simple interface without buttons realization (only Exit button is available).
    Also add test-background and music

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Game
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int musicbutton_count = 0;
+        WMPLib.WindowsMediaPlayer WMP = new WMPLib.WindowsMediaPlayer();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WMP.URL = @"D:\Visual Studio\Projects\Game\Music\song1.mp3";
+            WMP.controls.play();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void UIElement_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            if (musicbutton_count % 2 == 0)
+            {
+                WMP.controls.pause();
+                musicOnIcon.Visibility = Visibility.Hidden;
+                musicOffIcon.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                WMP.controls.play();
+                musicOffIcon.Visibility = Visibility.Hidden;
+                musicOnIcon.Visibility = Visibility.Visible;
+            }
+
+            musicbutton_count += 1;
+        }
+    }
+}
